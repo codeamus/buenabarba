613b0451702ccbb27e70b1098c1a0f336edcc10c
Alexander Urrutia <matias04041994@gmail.com>
2025-12-03 08:04:12 -0300

    Add Footer component with contact information and social links

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,1 +1,7 @@
 export const RESERVE_URL = "https://buenabarba.site.agendapro.com/cl/sucursal/77197";
+export const FB_URL = "https://www.facebook.com/people/Buena-barba-barberia/100084032545442/";
+export const IG_URL = "https://www.instagram.com/buenabarbabarberia/";
+export const PHONE = "+56 9 7653 3082";
+export const PHONE_HREF = "+56976533082";
+export const EMAIL = "buenabarbabarberia@gmail.com";
+export const ADDRESS = "Vicuña Mackenna Poniente 6650, local 5. La Florida";
